1b4a0c55567a602ad9a792af7b5492b10c655e8a
Natali Martynova <91197812+umewmew@users.noreply.github.com>
2024-05-29 14:00:47 +0500

    refactor: add modules

---
+++ js/generate-data.js
@@ -0,0 +1,38 @@
+import { getRandomPositiveInteger, getRandomArrayElement } from './utils.js';
+import {
+  ID,
+  URL,
+  DESCRIPTION,
+  MIN_LIKES,
+  MAX_LIKES,
+  AVATAR,
+  DESCRIPTION_PHOTO_COUNT,
+  MESSAGE_ARRAY,
+  NAME_USER,
+} from './data.js';
+
+const generateCommentsOfPhoto = () => ({
+  id: getRandomPositiveInteger(1, ID),
+  avatar: `img-avatar-${getRandomPositiveInteger(1, AVATAR)}.svg`,
+  message: getRandomArrayElement(MESSAGE_ARRAY),
+  name: getRandomArrayElement(NAME_USER),
+});
+
+const generateDescriptionOfPhoto = () => ({
+  id: getRandomPositiveInteger(1, ID),
+  url: `photos/${getRandomPositiveInteger(1, URL)}.jpg`,
+  description: `${DESCRIPTION}`,
+  likes: getRandomPositiveInteger(MIN_LIKES, MAX_LIKES),
+  comments: generateCommentsOfPhoto(),
+});
+
+const createDescriptionPhoto = Array.from(
+  { length: DESCRIPTION_PHOTO_COUNT },
+  generateDescriptionOfPhoto
+);
+
+generateCommentsOfPhoto();
+generateDescriptionOfPhoto();
+
+// eslint-disable-next-line no-console
+console.log(createDescriptionPhoto);
