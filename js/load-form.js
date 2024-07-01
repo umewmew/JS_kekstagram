79ee7bf4706f6a580d714bc911d8593c833e3c27
Natali Martynova <91197812+umewmew@users.noreply.github.com>
2024-07-01 18:40:05 +0500

    refactor: delete pristine

--- js/load-form.js
+++ js/load-form.js
@@ -1,5 +1,6 @@
 import { isEscapeKey } from './utils.js';
 const uploadForm = document.querySelector('.img-upload__form');
+const uploadFormHashtag = uploadForm.querySelector('.text__hashtags');
 const inputUploadFile = uploadForm.querySelector('.img-upload__input');
 const editForm = uploadForm.querySelector('.img-upload__overlay');
 const closeEditFormButton = uploadForm.querySelector('.img-upload__cancel');
@@ -17,6 +18,10 @@ function openEditForm() {
 
   closeEditFormButton.addEventListener('click', closeEditForm);
   document.addEventListener('keydown', onEscapeKeydown);
+
+  uploadFormHashtag.addEventListener('keydown', (evt) => {
+    evt.stopPropagation();
+  });
 }
 
 function closeEditForm() {
@@ -29,3 +34,30 @@ function closeEditForm() {
 }
 
 inputUploadFile.addEventListener('change', openEditForm);
+
+//валидация
+
+// const re = /^#[A-Za-zA-Яа-яЕё0-9]{1,19}$/i;
+// const pristine = new Pristine(uploadForm, {
+//   classTo: 'text__hashtags',
+//   errorClass: 'text__hashtags--invalid', // Класс, обозначающий невалидное поле
+//   successClass: 'text__hashtags--valid', // Класс, обозначающий валидное поле
+//   errorTextParent: 'text__hashtags', // Элемент, куда будет выводиться текст с ошибкой
+//   errorTextTag: 'span', // Тег, который будет обрамлять текст ошибки
+//   errorTextClass: 'form__error', // Класс для элемента с текстом ошибки
+// });
+
+// function validateHashtag(value) {
+//   return value[0] === '#';
+// }
+
+// pristine.addValidator(
+//   uploadForm.querySelector('.text__hashtags'),
+//   validateHashtag,
+//   'тут должна быть решетка'
+// );
+
+// uploadForm.addEventListener('submit', (evt) => {
+//   evt.preventDefault();
+//   pristine.vaidate();
+// });
--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ import './generate-data.js';
 import './show-miniatures-photo.js';
 import './show-big-photo.js';
 import './load-form.js';
+// import './validate-form.js';
